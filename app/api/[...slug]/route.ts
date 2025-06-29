2c0b4a62f951f57cae5b246b02d822e80977f842
yoo16 <yohei.yoshikawa@gmail.com>
2025-06-29 12:14:44 +0900

    update

--- app/api/[...slug]/route.ts
+++ app/api/[...slug]/route.ts
@@ -1,17 +1,21 @@
-import { NextRequest } from 'next/server';
-
-export async function GET(req: NextRequest, { params }: { params: { slug: string[] } }) {
-    const slugPath = params.slug.join('/');
+export async function GET(
+    request: Request,
+    { params }: { params: { slug: string[] } }
+) {
+    const slug = params.slug;
+    const slugPath = slug.join('/');
     const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || '';
     const phpApiUrl = `${API_BASE_URL}${slugPath}.php`;
-    console.log('Request URL:', API_BASE_URL);
-    console.log('PHP API URL:', phpApiUrl);
 
     try {
         const res = await fetch(phpApiUrl);
         const data = await res.json();
         return Response.json(data);
     } catch {
-        return new Response(JSON.stringify({ error: 'Failed to fetch' }), { status: 500 });
+        return new Response(JSON.stringify({
+            error: 'Failed to fetch',
+            slug: slug,
+            uri: phpApiUrl,
+        }), { status: 500 });
     }
 }
